84f70897b734d3e41efde8a3e1a700e7b9dd810d
VictorWqzx <workgruop038@gmail.com>
2024-11-19 09:16:10 +0300

    kyu codewars

---
+++ 5_kyu/ceaser.php
@@ -0,0 +1,42 @@
+<?php
+class CaesarCipher {
+    private $shift;
+
+    public function __construct($shift) {
+        $this->shift = $shift;
+    }
+
+    public function encode($input) {
+        $input = strtoupper($input);
+        $output = '';
+
+        for ($i = 0; $i < strlen($input); $i++) {
+            $char = $input[$i];
+            if (ctype_alpha($char)) {
+                $newChar = chr((ord($char) - ord('A') + $this->shift) % 26 + ord('A'));
+                $output .= $newChar;
+            } else {
+                $output .= $char;
+            }
+        }
+
+        return $output;
+    }
+
+    public function decode($input) {
+        $input = strtoupper($input);
+        $output = '';
+
+        for ($i = 0; $i < strlen($input); $i++) {
+            $char = $input[$i];
+            if (ctype_alpha($char)) {
+                $newChar = chr((ord($char) - ord('A') - $this->shift + 26) % 26 + ord('A'));
+                $output .= $newChar;
+            } else {
+                $output .= $char;
+            }
+        }
+
+        return $output;
+    }
+}
